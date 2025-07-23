d3515882e8ee3f2927f806f0710a0a059b58d7ea
Sofya <sofya@Sofyas-MacBook-Pro.local>
2025-07-23 12:54:32 +0500

    task description

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*Task 
+Make up an algorithm 
+If the entered number is greater than 7, then print “Hello” 
+If the entered name matches “John”, then output “Hello, John”, if not, then output "There is no such name" 
+There is a numeric array at the input, it is necessary to output array elements that are multiples of 3 */
+
+
+
+ 
+/*Answer the questions 
+Given bracket sequence: [((())()(())]] 
+Can this sequence be considered correct? 
+If the answer to the previous question is “no”, then what needs to be changed in it to make it correct? */
+ 
+/*Bonus points: 
++ the ability to run the received program 
+++ interactive input of values for processing and output of processed data 
+(for example, via the console) /*
